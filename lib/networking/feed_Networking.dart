15e96082fc9baddd34f0bc403de18abe661591b7
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-25 22:53:07 -0300

    create feed_networking

---
+++ lib/networking/feed_Networking.dart
@@ -0,0 +1,18 @@
+import 'package:webfeed/webfeed.dart';
+import 'package:http/http.dart' as http;
+
+class FeedNetworking {
+  Future<RssFeed> searchFeed() async {
+    var url = 'https://www.aviationwiki.net/feeds/posts/default?alt=rss';
+
+    var response = await http.get(url);
+    if (response == null) return null;
+
+    switch (response.statusCode) {
+      case 200:
+        return RssFeed.parse(response.body);
+      default:
+        throw Exception('Failed to getAvailableFees');
+    }
+  }
+}
