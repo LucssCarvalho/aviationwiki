783e1f394f7050cde942da7813b0c750a1833e87
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-04-25 22:52:51 -0300

    create feedItem

---
+++ lib/widgets/feedItem.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:webfeed/webfeed.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class FeedItem extends StatelessWidget {
+  final RssItem feedItem;
+
+  FeedItem(this.feedItem);
+  static const String placeholderImg = 'assets/images/no_img.png';
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          leading: thumbnail(feedItem.media.thumbnails.first.url),
+          title: Text(feedItem.title),
+        ),
+      ),
+    );
+  }
+
+  thumbnail(String imageUrl) {
+    return CachedNetworkImage(
+        placeholder: (context, url) => Image.asset(placeholderImg),
+        imageUrl: imageUrl,
+        height: 100,
+        width: 100,
+        alignment: Alignment.center,
+        fit: BoxFit.fill);
+  }
+}
