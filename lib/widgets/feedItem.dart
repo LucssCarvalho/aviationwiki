7c5af440233c200565a26df2782b29d612d09737
Lucas Carvalho <49245496+LucssCarvalho@users.noreply.github.com>
2020-05-02 23:51:45 -0300

    add route feedItem

--- lib/widgets/feedItem.dart
+++ lib/widgets/feedItem.dart
@@ -1,3 +1,4 @@
+import 'package:aviationwiki/screen/feedScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:webfeed/webfeed.dart';
 import 'package:cached_network_image/cached_network_image.dart';
@@ -10,12 +11,21 @@ class FeedItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: ListTile(
-          leading: thumbnail(feedItem.media.thumbnails.first.url),
-          title: Text(feedItem.title),
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => FeedScreen(feedItem),
+          ),
+        );
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            leading: thumbnail(feedItem.media.thumbnails.first.url),
+            title: Text(feedItem.title),
+          ),
         ),
       ),
     );
